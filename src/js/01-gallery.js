d2a176655345f7ea8965d2555bbb0ba20cb5265d
ritamirgorodska <ritamirgorodska.1604@gmail.com>
2022-12-02 13:45:14 +0300

    edit files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,21 @@
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
-const listGallery = document.querySelector('.gallery')
-const markUp = createGal(galleryItems)
-listGallery.innerHTML = markUp
-function createGal (galleryItems) {
-    return galleryItems.map(({preview, original, description})=>{
-        return `<a class="gallery__item" href="${original}">
+const listGallery = document.querySelector('.gallery');
+const markUp = createGallery(galleryItems);
+listGallery.innerHTML = markUp;
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}"/>
-      </a>`
-    }).join('')
+      </a>`;
+    })
+    .join('');
 }
-new SimpleLightbox ( '.gallery a' ,  {captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,} )
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,25 @@
-import throttle from 'lodash.throttle' 
+import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form')
+const formElement = document.querySelector('.feedback-form');
 
-formEl.addEventListener('input', throttle(onListenerInput, 500))
-formEl.addEventListener('submit', onReset)
+formElement.addEventListener('input', throttle(onListenerInput, 500));
+formElement.addEventListener('submit', onReset);
 
-function onListenerInput (e) {
-    if(e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
-        return
-    }
-    const formUser = {
-        email: formEl.email.value,
-        message: formEl.message.value
-    }
-    const formUserJSON = JSON.stringify(formUser)
-    localStorage.setItem("feedback-form-state", formUserJSON)
+function onListenerInput(e) {
+  if (e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'TEXTAREA') {
+    return;
+  }
+  const formUser = {
+    email: formElement.email.value,
+    message: formElement.message.value,
+  };
+  const formUserJSON = JSON.stringify(formUser);
+  localStorage.setItem('feedback-form-state', formUserJSON);
 }
 
-function onReset (e) {
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')))
-    e.preventDefault();
+function onReset(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
   e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
@@ -27,9 +27,9 @@ function onReset (e) {
 function LocalStorage() {
   const data = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (data) {
-    formEl.email.value = data.email;
-    formEl.message.value = data.message;
+    formElement.email.value = data.email;
+    formElement.message.value = data.message;
   }
 }
 
-LocalStorage()
+LocalStorage();
